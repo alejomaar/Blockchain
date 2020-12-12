974d466926480b4260b9ae407f18e8b03ae904ce
alejomar <demonmaar@gmail.com>
2020-12-12 01:11:27 -0500

    Data with Json format, and structure with a dictionaty, the sha 256 works with the data information

--- Blockchain.py
+++ Blockchain.py
@@ -1,25 +1,32 @@
 from random import *
+import hashlib
+import json
 
 class Data:
-    def __init__(self,name):
-        self.name = name
+    def __init__(self,name,cash):
+        self.Data = {'Name':name,'Cash':cash}
+    def JSON(self):
+        return json.dumps(self.Data)
 
 class Blockchain:
     def __init__(self):
         self.blocks = []#Block Origin
-        blockOrigin = Block(0,self.gethash(),0,Data("Camila"))
+        dataOrigin = Data("Camila",30)
+        blockOrigin = Block(0,self.gethash(dataOrigin),0,dataOrigin)
         self.blocks.append(blockOrigin)
-    def gethash(self):
-        return randint(1, 100)
+    def gethash(self,data):
+        return hashlib.md5(data.JSON().encode()).hexdigest()
+        #return randint(1, 100)
     def create(self,data):
-        block = Block(len(self.blocks),self.gethash(),self.prevhash(),data)
+        block = Block(len(self.blocks),self.gethash(data),self.prevhash(),data)
         self.blocks.append(block)
     def prevhash(self):
         return self.blocks[-1].H
 
     def show(self):
         for block in self.blocks:
-            print("I:",block.index,"#",block.H,"<-#",block.prev_hash,"Name",block.data.name)
+            print(block.data.JSON())
+            print("I:",block.index,"#",block.H,"<-#",block.prev_hash)
 
     
 class Block:
@@ -32,7 +39,7 @@ class Block:
 
     
 blockchain = Blockchain();
-blockchain.create(Data("Alejandro"));
-blockchain.create(Data("Fernando"));
-blockchain.create(Data("Sofia"));
+blockchain.create(Data("Alejandro",21));
+blockchain.create(Data("Fernando",51));
+blockchain.create(Data("Sofia",70));
 blockchain.show();
