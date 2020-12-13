354a06c7a77ac0626a0f0bd362d5f20bc573abeb
alejomar <demonmaar@gmail.com>
2020-12-13 16:20:37 -0500

    Blockchain with index, prevhash, and data encrypted, the proof of works still is need

--- Blockchain.py
+++ Blockchain.py
@@ -1,45 +1,60 @@
 from random import *
-import hashlib
+import hashlib 
 import json
 
 class Data:
     def __init__(self,name,cash):
         self.Data = {'Name':name,'Cash':cash}
-    def JSON(self):
-        return json.dumps(self.Data)
+    #def format(self):
+       # return self.Data.Name+"/"+self.Data.Cash
+    #return json.dumps(self.Data)
 
 class Blockchain:
     def __init__(self):
-        self.blocks = []#Block Origin
-        dataOrigin = Data("Camila",30)
-        blockOrigin = Block(0,self.gethash(dataOrigin),0,dataOrigin)
-        self.blocks.append(blockOrigin)
-    def gethash(self,data):
-        return hashlib.md5(data.JSON().encode()).hexdigest()
-        #return randint(1, 100)
+        self.blockchain = []#Block Origin
+        self.genesis()
+    def genesis(self):
+        genesis_data = Data("camila","30")
+        genesis_block = Block(str(len(self.blockchain)),'0'*64,genesis_data)
+        self.blockchain.append(genesis_block)
     def create(self,data):
-        block = Block(len(self.blocks),self.gethash(data),self.prevhash(),data)
-        self.blocks.append(block)
-    def prevhash(self):
-        return self.blocks[-1].H
-
+        block = Block(str(len(self.blockchain)),self.blockchain[-1].Block['hash'],data)
+        self.blockchain.append(block)
     def show(self):
-        for block in self.blocks:
-            print(block.data.JSON())
-            print("I:",block.index,"#",block.H,"<-#",block.prev_hash)
-
+        for block in self.blockchain:
+            block.show()
+    def showhash(self):
+        for block in self.blockchain:
+            block.showhash()
+            
     
 class Block:
-    def __init__(self,index,H,prev_hash,data):
-        self.index = index
-        self.H = H
-        self.prev_hash = prev_hash
-        self.data = data
+    def __init__(self,index,prev_hash,Data):
+        Hash = self.gethash(index,prev_hash,Data)
+        self.Block = {'index':index,'hash':Hash,'prev_hash':prev_hash,'data':Data.Data}
 
-
+    def gethash(self,index,prev_hash,Data):
+        Format= index+'/'+prev_hash+'/'+Data.Data['Name']+'/'+Data.Data['Cash']
+        Hash = self.encrypt(Format)
+        return Hash
+    def encrypt(self,Format):
+        return hashlib.sha256(Format.encode()).hexdigest()
     
+    def JSON(self):
+        return json.dumps(self.Block)
+    def show(self):
+        Block = self.Block
+        print(Block['index'],Block['data'])
+    def showhash(self):
+        Block = self.Block
+        print(Block['index'],Block['hash'],'-',Block['prev_hash'])
+    def showall(self):
+        print(self.JSON())
+
+
 blockchain = Blockchain();
-blockchain.create(Data("Alejandro",21));
-blockchain.create(Data("Fernando",51));
-blockchain.create(Data("Sofia",70));
-blockchain.show();
+blockchain.create(Data("Alejandro","21"))
+blockchain.create(Data("Fernando","51"))
+blockchain.create(Data("Sofia","21"))
+blockchain.showhash()
+
