40857e73b0be716f90783e91a8c23d165a75013b
alejomar <demonmaar@gmail.com>
2020-12-16 23:03:13 -0500

    add interfaz to new block added to blockchain

--- setup.py
+++ setup.py
@@ -1,22 +1,48 @@
-from flask.json import jsonify
+from flask.json import jsonify, request
 from Blockchain import *
 from flask import Flask,render_template,jsonify
 
 app =Flask(__name__)
+blockchain = Blockchain()
 
 @app.route('/')
 def home():
-    json = BC()
+    return render_template('index.html')
+
+@app.route('/add',methods=['POST'])
+def add():
+    global blockchain
+    if request.method == "POST":
+        req = request.form
+
+        name = req["Name"]
+        cash = req["Cash"]
+
+        blockchain.create(Data(name,cash))
+
+        json = blockchain.JSON()
+        #return jsonify({"m1":"data1","m2":"data2"})
+        return jsonify(json)
+        #return "success",201
+    else:
+        return "Invalid",404
+
+@app.route('/show')
+def show():
+    json = blockchain.JSON()
     return jsonify(json)
+
     #return render_template('index.html',json)
 
+'''def getchain():
+    json = blockchain.JSON()
+    return jsonify(json)
+
 def BC():
-    blockchain = Blockchain()
-    blockchain.create(Data("Alejandro","20"))
     blockchain.create(Data("Fernando","51"))
     blockchain.create(Data("Sofia","21"))
     blockchain.create(Data("Cris","1000000"))
-    json = blockchain.JSON()
-    return json
+    '''
+
 if __name__ == '__main__':
     app.run(debug=True)
